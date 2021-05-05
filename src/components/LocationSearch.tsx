158cd9c809c0dd9ac8fabd412dcf663665c7632b
Keaton <tkfoster26@gmail.com>
2021-05-05 17:52:58 -0600

    more boiler for input; cleanup

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -2,7 +2,12 @@ import React from "react";
 
 function LocationSearch() {
   return (
-    <div>pickles</div>
+    <input
+      id='locationInput'
+      type='text'
+      value=''
+      placeholder='City name'
+    />
   );
 }
 
