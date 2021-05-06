cf56802619dbdc7eed649e2587924faddb2f30d8
Keaton <tkfoster26@gmail.com>
2021-05-05 19:56:48 -0600

    fetching cities

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
+import axios from 'axios';
+
+
+async function fetchCityMatches(cityName: string): Promise<string> {
+  const url = `http://localhost:8001/city/?name=${cityName}`;
+  console.log('url', url);
+  const matched = await axios.get(url);
+  console.log('matched', matched);
+  return 'bro';
+}
 
 function LocationSearch() {
   const [inputValue, setInputValue] = useState('');
   const [isActive, setIsActive] = useState(false);
-  
+
   return (
     <input
       className={`input ${isActive && inputValue !== '' && 'active'}`}
@@ -12,6 +22,7 @@ function LocationSearch() {
       value={inputValue}
       placeholder='City name'
       onChange={(e) => {
+        fetchCityMatches(e.target.value);
         setInputValue(e.target.value);
       }}
       onFocus={() => setIsActive(true)}
