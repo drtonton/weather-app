6edaf6d856ac642ccab86ad23c40dcde9d224ac3
Keaton <tkfoster26@gmail.com>
2021-05-05 19:13:18 -0600

    added input styles

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function LocationSearch() {
+  const [inputValue, setInputValue] = useState('');
+  const [isActive, setIsActive] = useState(false);
+  
   return (
     <input
+      className={`input ${isActive && inputValue !== '' && 'active'}`}
       id='locationInput'
       type='text'
-      value=''
+      value={inputValue}
       placeholder='City name'
+      onChange={(e) => {
+        setInputValue(e.target.value);
+      }}
+      onFocus={() => setIsActive(true)}
     />
   );
 }
