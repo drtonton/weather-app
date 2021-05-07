476bc022059e8854292bb103feac4a1263b010a8
Keaton <tkfoster26@gmail.com>
2021-05-07 15:52:58 -0600

    refactoring

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -3,22 +3,44 @@ import axios from 'axios';
 
 let timerId: any;
 
-function fetchCityMatches(cityName: string): void {
-  const url = `http://localhost:8001/city/?name=${cityName}`;
-  axios.get(url);
-  return;
+interface City {
+  name: string;
 }
 
-function throttledCitySearch(searchValue: string): void {
-  if (timerId) return;
-  timerId = setTimeout(async() => {
-    await fetchCityMatches(searchValue);
-    timerId = undefined;
-  }, 500);
-}
+// function fetchCityMatches(cityName: string): void {
+//   const url = `http://localhost:8001/city/?name=${cityName}`;
+//   axios.get(url);
+// }
+
+// function throttledCitySearch(searchValue: string): void {
+//   if (timerId) return;
+//   timerId = setTimeout(async() => {
+//     await fetchCityMatches(searchValue);
+//     timerId = undefined;
+//   }, 500);
+// }
 
 function LocationSearch() {
   const [inputValue, setInputValue] = useState('');
+  const [matchedCities, setMatchedCities] = useState([]);
+
+  const throttledCitySearch = (searchValue: string): string | undefined => {
+    if (timerId) return;
+    timerId = setTimeout(async() => {
+      const matched = await fetchCityMatches(searchValue);
+      console.log('matched', matched);
+      // setMatchedCities(matched);
+      timerId = undefined;
+      return 'bro';
+    }, 100);
+  }
+
+  // todo: instead of any[], make a Type of the return shape and enforce it
+  const fetchCityMatches = async (cityName: string): Promise<any[]> => {
+    const url = `http://localhost:8001/city/?name=${cityName}`;
+    const cities = await axios.get(url);
+    return cities.data;
+  }
 
   return (
     <div className='fieldWrapper'>
