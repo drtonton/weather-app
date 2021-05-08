d9da2eeafbc17351ab667c32cbe96ddf345fa8d1
Keaton <tkfoster26@gmail.com>
2021-05-08 11:12:11 -0600

    fixed throttling

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from 'axios';
 
-let timerId: any;
+let timer: any;
 
 // todo: needs to go in a type file
 type City = {
@@ -15,19 +15,20 @@ function LocationSearch() {
   const [inputValue, setInputValue] = useState('');
   const [matchedCities, setMatchedCities] = useState<City[]>([]);
 
-  // todo: decide whether to do this yourself, or with lodash (must uninstall lodash if you choose this)
-  const throttledCitySearch = (searchValue: string): undefined => {
-    if (timerId) return;
-    timerId = setTimeout(async() => {
+  const throttledCitySearch = (searchValue: string): void => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(async() => {
       const matched = await fetchCityMatches(searchValue);
       setMatchedCities(matched);
-      timerId = undefined;
-      return;
-    }, 100);
+      timer = undefined;
+    }, 500);
   }
 
   // todo: instead of any[], make a Type of the return shape and enforce it
   const fetchCityMatches = async (cityName: string): Promise<any[]> => {
+    console.log('fetching: ', cityName);
     const url = `http://localhost:8001/city/?name=${cityName}`;
     const cities = await axios.get(url);
     return cities.data;
@@ -64,3 +65,12 @@ function LocationSearch() {
 }
 
 export default LocationSearch;
+
+
+/*
+-> type char
+-> function is invoked
+-> timer is 1
+-> after timer expires -> fetch
+-> 
+*/
