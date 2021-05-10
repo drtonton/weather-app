2319f3952cd664ab055f49dbca94c1f0408e4ce9
Keaton <tkfoster26@gmail.com>
2021-05-10 12:35:15 -0600

    added test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Rendering the empty state', () => {
+  test('renders location input', () => {
+    render(<App />);
+    const inputLabelText = screen.getByText('City Name (case sensitive)');
+    const inputPlaceholderText = screen.getByPlaceholderText('type here');
+    expect(inputLabelText).toBeInTheDocument();
+    expect(inputPlaceholderText).toBeInTheDocument();
+  });
+})
